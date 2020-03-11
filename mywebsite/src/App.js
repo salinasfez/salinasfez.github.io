9e5607269c36655ea689bc0b30e72f7616ce4eb6
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-03-11 18:55:55 -0500

    home page is almost done, i need to link the icons on the bottom

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -16,10 +16,10 @@ function App() {
           <NavLink className='nav-link' to='/'>Home</NavLink>
           <NavLink className='nav-link'  to='/projects'>Projects</NavLink>
           <NavLink className='nav-link'  to='/aboutme'>About Me</NavLink>
-          <NavLink className='nav-link'  to='/contact'>Contact</NavLink>
+          {/* <NavLink className='nav-link'  to='/contact'>Contact</NavLink> */}
         </Nav>
         <Route exact path='/' component={Main}/>
-        <Route exact path='/contact' component={Contact}/>
+        {/* <Route exact path='/contact' component={Contact}/> */}
         <Route exact path='/projects' component={Projects}/>
         <Route exact path='/aboutme' component={AboutMe}/>
     </BrowserRouter>
--- mywebsite/src/components/Main.js
+++ mywebsite/src/components/Main.js
@@ -6,20 +6,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Main extends React.Component{
+    // componentDidMount () {
+    //     const script = document.createElement("script");
+    
+    //     script.src = "https://platform.linkedin.com/badges/js/profile.js";
+    //     script.async = true;
+    
+    //     document.body.appendChild(script);
+    // }
     render(){
         return(
             <div>
-              
               <div className='container'>
-                    <img className='me item' id='main-image' src="https://lh3.googleusercontent.com/D99nSgB9ssZinHq6q5cX9oegT-D-34_VZ2U2cey1ghtynXWeQ3KJhT1jW11ejO7UBNqx6YLc3mqqahO2LBI9rRIWgqOMvrCtE3glogCT6m9bkIaVYP729Ro8DqN_wEJ439cOUOgCiKerBiURQvTl3U5VedyUQsL8S43r52HHBAlmIfC3UOrHhxDSdbgUSWie88ZphatNTMyw3ahAzunTo1UM5j-pNLqnpZlfA9yk2O2tfy5Z4TnniG_m8XfHPoR7cQqXL8Jlo9FR1QG2_U40qvN3_wMO61H80eaNT_EIl0duYW0WK-0mwWoc4Hp7B-cCm1qNbJxFrzEjd-Yji8YTcdjDjr2SAPY2iD8aMYK_Kcup3r8mCd4LWv2GgO7JPiJcneJ8XSUide2iow5IuuE385p2TJQws3OWXvdrA9S65hfClsEqBufzU4-azSJ9MM7locp7FsNTfyofUaCWSpQrOfQig25N1sHKjUEhYwz787jDYKywtHSlzcJgaP99atzltkYS_6eSTYTTy5D5Czrex1diKw8pctVIMSl2vFoKQM1F0DeVAX6RbtStRNX3xjMq5a10WM4CMMXjar70goo01safVyYKb3vJg7HSbKo-7ZZJ6jigFCh_s82Bn1e0UWy9BQhmiCnMHgbV56anQrrlgP4nHeg-7Gm88EU8EaYjdxt74u6UnUn_Iw=s745-no" alt="background" />
+                    <img className='me item' id='main-image' src={require('../images/bg.jpg')} alt="background" />
                 <div className='center-item'>
                     <h1 className='item who-i-am'>Federico Salinas || <span id='job-description'>Full-Stack Developer</span></h1>
+                    <p id='technologies-main'>HTML 5 | MongoDB | jQuery | Javascript | React | Express | CSS3 | Bootstrap</p>
+                <div className='icon-container'>
+                    <img src={require('../images/linkedin.png')} alt={'linkedin'} className='icons'/>
+                    <img src={require('../images/github-icon.png')} alt={'github'} className='icons'/>
+                    <img src={require('../images/email-icon.svg')} alt={'github'} className='icons'/>
+                    <img src={require('../images/phone-icon.png')} alt={'github'} className='icons'/>
+                    <img src={require('../images/resume-icon.svg')} alt={'github'} className='icons'/>
+                </div>
                 </div>
             </div>
-            
-                <div className='flex-item' id='technologies'>
-                    HTML 5 | MongoDB | jQuery | Javascript | React | Express | CSS3 | Bootstrap
-                </div>
             </div>
         )
     }
