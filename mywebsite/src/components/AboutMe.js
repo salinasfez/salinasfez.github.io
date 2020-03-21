a14f98bb59e8ecc8494a468b68cda680dc171998
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-03-21 09:57:03 -0500

    worked on my about me page

--- mywebsite/src/components/AboutMe.js
+++ mywebsite/src/components/AboutMe.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './AboutMe.css';
+import resume from '../images/federico_salinas_resume.pdf'
+import {Button} from 'react-bootstrap';
 class AboutMe extends React.Component{
     
     render(){
         return(
             <div className='flex-container'>
-                <div id='paragraph' className='flex-item'>
-                Software developer committed to creating the most efficient solutions to routine methods in order to save companies money and provide users with a seamless experience. I focus on leading by example and keeping my team moving forward. In the workplace, I believe that by supporting and elevating my coworkers, we all find greater success. Honesty, hard work, and gratitude are my lifestyle. 
-                </div>
                 <div className='flex-item' id='technologies'>
+                    <h4>Technologies</h4>
                     HTML 5 | MongoDB | jQuery | Javascript | React | Express | CSS3 | Bootstrap
                 </div>
-                <div className='flex-item'>
-                {/* <img src={require('/images/image-name.png')} /> */}
-                <img src={require('../images/profilepic.jpg')} alt={'federico'} className='federico' />
+                <div id='paragraph' className='flex-item'>
+                {/* Software developer committed to creating the most efficient solutions to routine methods in order to save companies money and provide users with a seamless experience. I focus on leading by example and keeping my team moving forward. In the workplace, I believe that by supporting and elevating my coworkers, we all find greater success. Honesty, hard work, and gratitude are my lifestyle.  */}
+                    <p>I am a software developer looking to fully emerge into the industry. I graduated from General Assembly's Software Engineering Immersive Remote program at the end of February 2020 and also attended Mchenry County College, where I enrolled in their Computer Information Technologies program. </p>
+                    <p>
+                        I am currently a bakery and deli manager downtown Chicago. I have improved my departments' sales percentages and overall presentation of the products we offer.
+                        I have been in management for over 7 years and adapted with success between the two jobs I've had in the past 12 years.
+                    </p>
+                    <p>
+                        At General Assembly I was faced with challenges professionals face in the real world. I worked both individually and in teams to accomplish CRUD functionality projects. 
+                    </p>
+
                 </div>
+                <div id='download-resume-section'>
+                    <a id='resume-link' download='federico_salinas_resume.pdf' href={resume}><Button>Download Resume</Button></a>
+                    <a id='resume-link' download='federico_salinas_resume.pdf' href={resume}><img width={'20%'} height={'20%'} src={require('../images/resume-icon.svg')} alt={'github'}/></a>
+                </div>
+          
             </div>
         )
     }
--- mywebsite/src/components/Projects.js
+++ mywebsite/src/components/Projects.js
@@ -8,24 +8,32 @@ import './Projects.css';
 
 
 class Projects extends React.Component{
+ 
     render(){
-        return(
+
+    return(
             <div className='flex-container'>   
                     <h1 id='main-header'>Mercy Clinic Appointment Center</h1>
                   
                       
                         <div className='image-container'>
-                            <img src={require('../images/mercy_home.png')} className='images' />
-                            <img src={require('../images/mercy_book.png')} className='images'/>
-                            <img src={require('../images/mercy_details.png')} className='images'/>
+                            
+                                <img src={require('../images/mercy_home.png')} className='images'/>
+                            
+                            
+                                <img src={require('../images/mercy_book.png')} className='images'/>
+                          
+                           
+                                <img src={require('../images/mercy_details.png')} className='images'/>
+                            
+                           
                         </div>
-
                         
                            
                         
                         <div id='paragraph'>
-                            Most apps need to know the identity of a user. Knowing a user's identity allows an app to securely save user data in the cloud and provide the same personalized experience across all of the user's devices.
-                            Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
+                            Mercy Clinic Appointment Center was an idea born when I was booking my appointment with my doctor whom does not have online booking. I have to call everytime, ask for availability, and the worst part is I have to call during open hours. Through an app like this one, I can book, cancel, or update my appointment at my own convenience. 
+                            <br/><br/><a className='project-links' href={'https://mercy-appointment-center.herokuapp.com/appointments'} target='_blank'>Project link...</a>
                         </div>
                     </div>
             
